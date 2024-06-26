73a5641abc6012c8ee8d07393f3664c47f204212
t1ww <95153968+t1ww@users.noreply.github.com>
2024-06-26 23:00:19 +0700

    commit 8.3

--- main.js
+++ main.js
@@ -11,17 +11,30 @@ createApp({
         const productLink = ref("https://www.camt.cmu.ac.th/");
         const sale = ref(false);
         const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+        // variants
         const variants = ref([
-            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 },
         ]);
+        const selectedVariant = ref(0);
+        function updateVariant(_index) {
+            selectedVariant.value = _index;
+        }
+        const image = computed(()=> {
+            return variants.value[selectedVariant.value].image;
+        })
+        const inStock = computed(()=> {
+            return variants.value[selectedVariant.value].quantity;
+        })
+
+        // size
         const sizes = ref(["S", "M", "L"]);
         const cart = ref(0);
         function addToCart() {
             cart.value++;
         }
-        function updateImage(variant_image) {
-            image.value = variant_image;
+        function updateImage(_variant_image) {
+            image.value = _variant_image;
         }
         function toggleInStock() {
             inStock.value = !inStock.value;
@@ -31,6 +44,6 @@ createApp({
             return brand.value + ' ' + product.value;
         })
         // return
-        return { title, desc, image, productLink, inStock, inventory, sale, details, variants, sizes, cart, addToCart, updateImage, toggleInStock };
+        return { title, desc, image, productLink, inStock, inventory, sale, details, variants, sizes, cart, addToCart, updateImage, updateVariant, toggleInStock };
     },
 }).mount("#app");
