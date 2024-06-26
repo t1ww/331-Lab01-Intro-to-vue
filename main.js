2ced9fe1ded947c2d20e5fc0a9c3bbdc17393091
t1ww <nongtews@gmail.com>
2024-06-26 16:05:32 +0700

    commit 6.6

--- main.js
+++ main.js
@@ -11,15 +11,18 @@ createApp({
         const sale = ref(false);
         const details = ref(["50% cotton", "30% wool", "20% polyester"]);
         const variants = ref([
-            { id: 2234, color: "green" },
-            { id: 2235, color: "blue" },
+            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
         ]);
         const sizes = ref(["S", "M", "L"]);
         const cart = ref(0);
         function add_to_cart() {
             cart.value++;
         }
+        function update_image(variant_image) {
+            image.value = variant_image;
+        }
         // return
-        return { product, desc, image, product_link, in_stock, inventory, sale, details, variants, sizes, cart, add_to_cart };
+        return { product, desc, image, product_link, in_stock, inventory, sale, details, variants, sizes, cart, add_to_cart, update_image };
     },
 }).mount("#app");
