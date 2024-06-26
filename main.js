426a4b1ee9539803e3791fb900209a5fe8057078
t1ww <nongtews@gmail.com>
2024-06-26 11:28:01 +0700

    commit 4.8

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ createApp({
         const product = ref("Boots");
         const desc = ref("Wears on both feet, keeps you warm");
         const image = ref("./assets/images/socks_green.jpg");
+        const in_stock = ref(true);
+        const inventory = ref(100);
         const product_link = ref("https://www.camt.cmu.ac.th/");
-        return { product, desc, image, product_link };
+        return { product, desc, image, product_link, in_stock, inventory };
     },
 }).mount("#app");
