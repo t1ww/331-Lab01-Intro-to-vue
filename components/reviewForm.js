46a8286e45ca0b0f6b2376aff8a57e9b763af15c
t1ww <95153968+t1ww@users.noreply.github.com>
2024-06-27 23:53:24 +0700

    12.2

--- components/reviewForm.js
+++ components/reviewForm.js
@@ -3,13 +3,13 @@ const reviewForm = {
     <form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name" v-model="form.name">
+        <input id="name" v-model="name">
 
         <label for="review">Review:</label>
-        <textarea id="review" v-model="form.review"></textarea>
+        <textarea id="review" v-model="review"></textarea>
 
         <label for="rating">Rating:</label>
-        <select id="rating" v-model.number="form.rating">
+        <select id="rating" v-model.number="rating">
             <option>5</option>
             <option>4</option>
             <option>3</option>
@@ -18,7 +18,7 @@ const reviewForm = {
         </select>
 
         <label for="recommend">Would you recommend this product?</label>
-        <select id="recommend" v-model="form.recommend">
+        <select id="recommend" v-model="recommend">
             <option>Yes</option>
             <option>No</option>
         </select>
